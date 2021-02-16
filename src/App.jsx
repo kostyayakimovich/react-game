dc0bff173573b007cb182970ce5465b35d9e61c1
Yakimovich Kanstantsin <yakimovichkostya@gmail.com>
2021-02-16 21:46:53 +0300

    feat: add footer, fullscreen, eslint

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import { Provider } from 'react-redux';
 
 import store from './store';
 import MemoryGameContainer from './containers/MemoryGameContainer';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <Provider store={store}>
+      <>
       <div className="App">
         <MemoryGameContainer />
+             <Footer/>   
       </div>
+      
+      </>
     </Provider>
   );
 }
--- src/containers/MainContainer/MainContainer.jsx
+++ src/containers/MainContainer/MainContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import Congratulations from '../../components/Congratulations';
 import {
   resetCardsField, initGame,
@@ -8,8 +8,11 @@ import {
 import BackButton from '../../components/BackButton';
 import CardsFieldContainer from '../CardsFieldContainer';
 import TimerContainer from '../TimerContainer/TimerContainer';
+import fullscreen from '../../assets/img/fullscreen.png'
+import styles from './styles.module.css';
 
 export default function MainContainer() {
+  const handle = useFullScreenHandle();
   const dispatch = useDispatch();
   const isWin = useSelector((state) => state.cardsReducer.isWin);
 
@@ -23,12 +26,17 @@ export default function MainContainer() {
 
   return (
     <>
+    <img alt = 'fullscreen' src ={fullscreen} onClick={handle.enter}  className={styles.imgFullScreen}/>
+    
+      <FullScreen className = {styles.fullScreenContainer} handle={handle}>
       {isWin ? <Congratulations />
         : (
           <CardsFieldContainer />
         )}
       <TimerContainer isWin={isWin} />
+     
       <BackButton onClick={goBack} />
+      </FullScreen>
     </>
   );
 }
